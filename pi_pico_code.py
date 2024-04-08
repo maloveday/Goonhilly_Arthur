566bfe24062d1aa596c0b8f644260994304bcf4a
maloveday <maloveday@googlemail.com>
2024-04-08 23:27:36 +0100

    Get ISS details from api and print lat, long and crew

--- pi_pico_code.py
+++ pi_pico_code.py
@@ -2,13 +2,17 @@ import network
 import socket
 import machine
 import requests
+import json
 from time import sleep
 from picozero import pico_led
 from secrets import ssid, password
+from math import radians, sin, cos, sqrt, atan2
 
 
 def connect():
-    #Connect to WLAN
+    """
+    Connect to WLAN
+    """
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
@@ -17,14 +21,26 @@ def connect():
         sleep(1)
     ip = wlan.ifconfig()[0]
     print(f'Connected on {ip}')
-    return ip
+    
+
+def iss():
+    """
+    Get ISS data from open-notify.org
+    """
+    res = requests.get(url='http://api.open-notify.org/iss-now.json')
+    iss_details = json.loads(res.text)
+    longitude = iss_details['iss_position']['longitude']
+    latitude = iss_details['iss_position']['latitude']
+    res = requests.get(url='http://api.open-notify.org/astros.json')
+    iss_crew = json.loads(res.text)
+    number = iss_crew['number']
+    print(f'ISS longitude is {longitude} and latitude is {latitude}')
+    print(f'ISS crew number is {number}')
+    return longitude, latitude, number
 
 
 try:
-    ip = connect()
-    res = requests.get(url='http://api.open-notify.org/iss-now.json')
-    print(res.text)
-    res = requests.get(url='http://api.open-notify.org/astros.json')
-    print(res.text)
+    connect()
+    iss()
 except KeyboardInterrupt:
     machine.reset()
